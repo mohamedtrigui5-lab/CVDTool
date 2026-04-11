8a03dd6aa401038c1cc1ed309d213f5812c5da97
mohamedtrigui5-lab <mohamedtrigui5@gmail.com>
2026-04-11 02:08:13 +0100

    Update FileCVD.cpp

--- FileCVD.cpp
+++ FileCVD.cpp
@@ -31,14 +31,20 @@ std::vector<ColorBlock> FileCVD::load(const std::string& filename, int& width, i
     std::vector<ColorBlock> result;
     if (!f) return result;
 
-    char sig[3]; f.read(sig, 3);
+    char sig[3];
+    f.read(sig, 3); // "CVD"
+
     uint32_t w, h, count;
-    f.read((char*)&w, 4); f.read((char*)&h, 4);
+    f.read((char*)&w, 4);
+    f.read((char*)&h, 4);
     f.read((char*)&count, 4);
-    width = w; height = h;
+
+    width = w;
+    height = h;
 
     for (uint32_t i = 0; i < count; i++) {
         ColorBlock b;
+        // Lecture RGBA (4 octets)
         f.read((char*)&b.color.r, 1);
         f.read((char*)&b.color.g, 1);
         f.read((char*)&b.color.b, 1);
@@ -46,9 +52,13 @@ std::vector<ColorBlock> FileCVD::load(const std::string& filename, int& width, i
 
         uint32_t streamSize;
         f.read((char*)&streamSize, 4);
-        b.bitstream.resize(streamSize);
-        f.read((char*)b.bitstream.data(), streamSize);
-        result.push_back(b);
+
+        // Sécurité : évite d'allouer trop de RAM si le fichier est corrompu
+        if (streamSize > 0 && streamSize < 10000000) {
+            b.bitstream.resize(streamSize);
+            f.read((char*)b.bitstream.data(), streamSize);
+            result.push_back(b);
+        }
     }
     return result;
 }
